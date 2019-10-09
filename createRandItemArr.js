c47de83526b57ee1e1d32f21aa62e4cbbfc84a9f
sergiy <aserg1234@yandex.ru>
2019-10-10 00:59:13 +0300

    checking agruments for errors was added

--- createRandItemArr.js
+++ createRandItemArr.js
@@ -1,7 +1,21 @@
 /////////////////////////////////////////////////////////////////////
-//функция создающая массив элементов(где элементы расположены в случайном
-//порядке) из др массива
+//the function creates and returns an array of random elements(arranged
+//in random order) from another array
+//функция создает и возвращает массив элементов(где элементы расположены 
+//в случайном порядке) из др массива
 function createRandItemArr(arr, randFunc) {
+
+	//checking arguments for errors
+	if(!Array.isArray(arr)) {
+		console.warn('the first argument is not an array!');
+		return;
+	}
+	if( typeof randFunc !== 'function' ) {
+		console.warn('the second argument is not a function!');
+		return;
+	}
+	////////////////////
+
 	const cache = [];
 	const newArr = arr.slice();
 	const length = newArr.length-1;
